bc05e8a733aa37007f457328723e96e972724137
aenalemmea <118059484+aenlemmea@users.noreply.github.com>
2023-12-29 16:49:14 +0530

    Did some cleaning up.

--- project.py
+++ project.py
@@ -6,11 +6,6 @@ import hashlib
 import random
 from datetime import datetime
 
-#generating unix time stamp 
-current_time = datetime.now()
-unix_timestamp=int(current_time.timestamp())
-
-#print("Current System Time:", unix_timestamp)
 
 #loading Env's
 load_dotenv()
@@ -18,27 +13,27 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 api_secret = os.getenv("secret")
 
-#print(f"API Key: {api_key}")
-#print(f"API Secret: {debug_mode}")
+def getContestList():
+	unix_timestamp = int(datetime.now().timestamp())
+	
+	#print(f"API Key: {api_key}")
+	#print(f"API Secret: {debug_mode}")
 
-method_name="contest.list"
-rand = str(random.randint(100000, 999999))
+	method_name="contest.list"
+	rand = str(random.randint(100000, 999999))
+	#constructing the string to be hashed 
 
-#constructing the string to be hashed 
-hash1=f"{rand}/{method_name}?apiKey={api_key}&time={unix_timestamp}#{api_secret}"
+	hash1=f"{rand}/{method_name}?apiKey={api_key}&time={unix_timestamp}#{api_secret}"
+	hash_code=hashlib.sha512(hash1.encode()).hexdigest()
 
-# SHA-512 hash
-hash_code=hashlib.sha512(hash1.encode()).hexdigest()
+	api_url = f"https://codeforces.com/api/{method_name}?apiKey={api_key}&time={unix_timestamp}&apiSig={rand}{hash_code}"
 
+	#Make the Api request 
+	response=requests.get(api_url)
+	print(response.json())
 
+# pretty print conetst list
+def pp_contestlist():
+	pass
 
-#constructing the url
-api_url = f"https://codeforces.com/api/{method_name}?apiKey={api_key}&time={unix_timestamp}&apiSig={rand}{hash_code}"
-
-
-
-#Make the Api request 
-
-response=requests.get(api_url)
-print(response.json())
-
+if __name__ == 
--- readme.txt
+++ readme.txt
@@ -1,1 +1,24 @@
-This is a bot made for discord server 
+pbot
+====
+
+General purpose bot for discord with functionality like:
+	1. Notify for upcoming programming contests for 
+		codechef, codeforces, geeksforgeeks.
+	2. Send math questions for probability topic.
+	3. Interface to chatGPT. 
+
+
+Contributing Setup:
+
+	1. Setup a virtual environment:
+		`python -m venv phenenv`
+	2. Activate the virtual environment with:
+		`.\phenenv\Scripts\Activate.ps1`
+	3. Install requirements:
+		`pip install -r requirements`
+	4. Develop!
+
+Contributors:
+	Shankhadeep Mandal 
+	Subhojit Mandal
+
