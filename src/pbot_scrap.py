1aa3e97cb16c0c85b76c9d58c8c2f013192c1de3
Subhojit131 <subhojitmandal147@gmail.com>
2023-12-29 22:28:29 +0530

    scraper for codechef

---
+++ src/pbot_scrap.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+
+website = 'https://www.codechef.com/contests'
+s = Service("/home/subhojit/discordbot/pbot/src/chromedriver")
+
+driver = webdriver.Chrome(service=s)
+
+driver.get(website)
+
+# Use WebDriverWait to wait for the tbody element to be present
+wait = WebDriverWait(driver, 10)
+tbody_element = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[4]/div/div/div[3]/div[2]/div[2]/div/div[2]/table/tbody")))
+
+# Find all rows inside the tbody
+rows = tbody_element.find_elements(By.TAG_NAME, 'tr')
+
+# Iterate through each row and print the text content of cells
+for row in rows:
+    cells = row.find_elements(By.TAG_NAME, 'td')  
+    row_data = [cell.text.strip() for cell in cells]
+    print(row_data)
+
+
+driver.quit()
