21d4bd0baca4351ee95dc18a516218bb38184d6f
aenlemmea <118059484+aenlemmea@users.noreply.github.com>
2024-01-03 11:49:11 +0530

    Get upcoming contests list from the codechef api

--- src/pbot_main.py.py
+++ src/pbot_main.py.py
@@ -1,7 +1,7 @@
 #setting up everything 
 from dotenv import load_dotenv
 import os
-import requests
+import requests as rq
 import hashlib
 import random
 from datetime import datetime
@@ -12,29 +12,18 @@ load_dotenv()
 
 api_key = os.getenv("API_KEY")
 api_secret = os.getenv("secret")
+cf_base_url = 'https://codeforces.com/api/'
 
 def getContestList():
-	unix_timestamp = int(datetime.now().timestamp())
-	
-	#print(f"API Key: {api_key}")
-	#print(f"API Secret: {debug_mode}")
-
-	method_name="contest.list"
-	rand = str(random.randint(100000, 999999))
-	#constructing the string to be hashed 
-
-	hash1=f"{rand}/{method_name}?apiKey={api_key}&time={unix_timestamp}#{api_secret}"
-	hash_code=hashlib.sha512(hash1.encode()).hexdigest()
-
-	api_url = f"https://codeforces.com/api/{method_name}?apiKey={api_key}&time={unix_timestamp}&apiSig={rand}{hash_code}"
-
-	#Make the Api request 
-	response=requests.get(api_url)
-	print(response.json())
+    
+    r = rq.get(cf_base_url + 'contest.list') # Warning. Big List
+    for res in r.json()["result"]:
+        if res["phase"] == "BEFORE":
+            print(res)
 
 # pretty print conetst list
 def pp_contestlist():
-	pass
+    pass
 
 if __name__ == '__main__':
-	
+    getContestList()
