26eca8d4715c8892afe1ffbec1842ca7538e56c6
aenlemmea <118059484+aenlemmea@users.noreply.github.com>
2024-01-03 16:20:37 +0530

    Separated the logic for the handler. Began work on the pretty printer for the  contest

--- src/handlers/codeforces_handler.py
+++ src/handlers/codeforces_handler.py
@@ -1,17 +1,6 @@
 #setting up everything 
-from dotenv import load_dotenv
-import os
 import requests as rq
-import hashlib
-import random
-from datetime import datetime
 
-
-#loading Env's
-load_dotenv()
-
-api_key = os.getenv("API_KEY")
-api_secret = os.getenv("secret")
 cf_base_url = 'https://codeforces.com/api/'
 
 def getContestList() -> list:
@@ -25,8 +14,9 @@ def getContestList() -> list:
     return fres
 
 # pretty print conetst list
-def pp_contestlist():
-    pass
+def pp_contestlist(reslist):
+    for i in reslist:
+        print(i)
 
 if __name__ == '__main__':
-    print(getContestList())
+    pp_contestlist(getContestList())
--- src/model/contest.py
+++ src/model/contest.py
@@ -12,25 +12,25 @@ class Contest:
     Enums are to be Tuple defined (due to immutability) 
     '''
 
-    id : int = 1
-    name : str = ""
+    id : int
+    name : str = "Contest"
     type = (CF, IOI, ICPC)
     phase = (BEFORE, CODING, PENDING_SYSTEM_TEST, SYSTEM_TEST, FINISHED)
-    frozen : bool = True
-    durationSeconds : int = 120 * 60
-    startTimeSeconds : int = 2403390598 # Unix Format Start Time <Optional>
-    relativeTimeSeconds : int = 34354654 # Can be negative <Optional>
-    preparedBy : str = "rui" # <Optional>
-    websiteUrl : str = "www.holaforces.com" # <Optional>
-    description : str = "Local" # <Optional>
-    difficulty : int = 1 # <Optional> From 1 to 5. Increasing difficulty
+    frozen : bool
+    durationSeconds : int
+    startTimeSeconds : int # Unix Format Start Time <Optional>
+    relativeTimeSeconds : int # Can be negative <Optional>
+    preparedBy : str  # <Optional>
+    websiteUrl : str  # <Optional>
+    description : str # <Optional>
+    difficulty : int # <Optional> From 1 to 5. Increasing difficulty
     
-    kind : str = "" # <Optional> 
+    kind : str # <Optional> 
     
     ''' @kind -> Official ICPC Contest, Official School Contest, Opencup Contest, School/University/City/Region Championship, Training Camp Contest, Official International Personal Contest, Training Contest.
     '''
 
-    icpcRegion : str = "" # Region for official ICPC contests. <Optional>
-    country : str = "India" # <Optional>
-    city : str = "Bagdogra" # <Optional>
-    season : str = "idk" # <Optional>
+    icpcRegion : str  # Region for official ICPC contests. <Optional>
+    country : str # <Optional>
+    city : str # <Optional>
+    season : str # <Optional>
